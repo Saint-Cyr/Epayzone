cc204b6ca6d4eccb178892fbdc1687e6c2e23613
S@int-Cyr <mapoukacyr@yahoo.fr>
2016-10-16 17:38:09 +0000

    update templates for fos_user

---
+++ src/EpayZoneBundle/EventListener/RegistrationListener.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace EpayZoneBundle\EventListener;
+
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+/**
+ * Listener responsible to change the redirection at the end of the password resetting
+ */
+class RegistrationListener implements EventSubscriberInterface
+{
+    private $router;
+
+    public function __construct(UrlGeneratorInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FOSUserEvents::REGISTRATION_SUCCESS => 'onRegistrationSuccess',
+        );
+    }
+
+    public function onRegistrationSuccess(FormEvent $event)
+    {
+        $url = $this->router->generate('epayzone_homepage');
+
+        $event->setResponse(new RedirectResponse($url));
+    }
+}
